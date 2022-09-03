ef4e096b8224736ee6be34ec15cb1323fb4f62e9
samantha <samanthax0326@gmail.com>
2022-09-02 23:50:40 -0700

    updated styling and javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// Sticky Navbar Feature
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "0px 10px";
+} else {
+    document.getElementById("navbar").style.padding = "60px 10px";
+  }
+} 
+
+
+// Modal
+
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
@@ -37,3 +52,5 @@ document.addEventListener("click", function(event) {
         closeForm()
     }
 }, false)
+
+
