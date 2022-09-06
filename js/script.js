81e0b647b552fe3537995ea9fa8d37643abfdaa9
samantha <samanthax0326@gmail.com>
2022-09-05 21:38:51 -0700

    linux update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// Sticky Navbar Feature
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("nav-links").style.padding = "7px";
-    document.getElementById("name").style.padding = "0px";
-    // document.getElementById("name").style.font-size = "5px";
-} else {
-    document.getElementById("navbar").style.padding = "10px 10px";
-
-  }
-} 
-
 
 // Modal
 function openForm() {
